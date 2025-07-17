f936823133719be5d07c5f21596f84e4605beaaa
Stefan Cotos <stefancotos8@gmail.com>
2025-07-17 17:12:47 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,14 +3,14 @@ from fastapi.staticfiles import StaticFiles
 from app.api import calculator, math_api
 from app.db.session import init_db
 
-app = FastAPI(title="Calculator Microservice")
+
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+
+app = FastAPI(title="Calculator Microservice", lifespan=lifespan)
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 app.include_router(calculator.router)
 app.include_router(math_api.router, prefix="/api")
-
-
-@app.on_event("startup")
-async def on_startup():
-    await init_db()
