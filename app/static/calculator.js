2aebad38ebd85a805a6d5bd2c64495a80f6944b7
Stefan Cotos <stefancotos8@gmail.com>
2025-07-21 09:18:10 +0300

    Remove dark/light theme

--- app/static/calculator.js
+++ app/static/calculator.js
@@ -110,15 +110,3 @@ async function deleteHistory() {
 
 document.addEventListener("DOMContentLoaded", loadHistory);
 
-
-function toggleTheme() {
-    const body = document.body;
-
-    if (body.classList.contains("dark-theme")) {
-        body.classList.remove("dark-theme");
-        body.classList.add("light-theme");
-    } else {
-        body.classList.remove("light-theme");
-        body.classList.add("dark-theme");
-    }
-}
